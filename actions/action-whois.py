5004a3aae3ac68dba77e231d4152c0d715d848e2
yaleman <yaleman@ricetek.net>
2019-02-27 17:09:22 +1000

    adding debug

--- actions/action-whois.py
+++ actions/action-whois.py
@@ -20,8 +20,10 @@ class Whois(Action):
         parsed_uri = urlparse(query)
         # if it's an IP address
         if parsed_uri.netloc == '' and re.match('^\d{,3}\.\d{,3}\.\d{,3}\.\d{,3}$', query):
+            print("whois on {}".format(query))
             w = whois(query)
         else:
+            print('whois on {}'.format(parsed.uri.netloc))
             w = whois(parsed_uri.netloc)
 
         return w
