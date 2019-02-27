e49ddbaf02b9601dc5df62814cada69973e797ef
yaleman <yaleman@ricetek.net>
2019-02-27 13:51:08 +1000

    fat fingered a value

--- actions/whois.py
+++ actions/whois.py
@@ -6,10 +6,10 @@ from st2common.runners.base_action import Action
 
 
 class Whois(Action):
-    def run(self, hostname):
+    def run(self, query):
         #result = {
         #    'pack_group': utils.get_pack_group(),
         #    'pack_path': utils.get_system_packs_base_path()
         #}
 
-        return "This should be a result, whois for {}".format(hostname)
+        return "This should be a result, whois for {}".format(query)
