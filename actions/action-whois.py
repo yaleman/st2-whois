d65ba2eb8480aa24b452e0bc39419ae197eea189
yaleman <yaleman@ricetek.net>
2019-02-27 16:47:35 +1000

    handling IPv4 addresses

--- actions/action-whois.py
+++ actions/action-whois.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
+import re
+from urllib.parse import urlparse
 
 from st2common.content import utils
 from st2common.runners.base_action import Action
 
 from whois import whois
 
-from urllib.parse import urlparse
 class Whois(Action):
     def run(self, query, cmd, *args):
 
@@ -14,7 +15,10 @@ class Whois(Action):
         query = query.replace("]", "")
 
         parsed_uri = urlparse(query)
-
-        w = whois(parsed_uri.netloc)
+        # if it's an IP address
+        if parsed_uri.netloc == '' and re.match('^\d{,3}\.\d{,3}\.\d{,3}\.\d{,3}$', query):
+            w = whois(query)
+        else:
+            w = whois(parsed_uri.netloc)
 
         return w
