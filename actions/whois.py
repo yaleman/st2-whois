b57ca5eed4abfb432b2f8d95bb313cf4796d29b0
yaleman <yaleman@ricetek.net>
2019-02-27 13:49:47 +1000

    moving to python whois

---
+++ actions/whois.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+from st2common.content import utils
+from st2common.runners.base_action import Action
+
+
+class Whois(Action):
+    def run(self, hostname):
+        #result = {
+        #    'pack_group': utils.get_pack_group(),
+        #    'pack_path': utils.get_system_packs_base_path()
+        #}
+
+        return "This should be a result, whois for {}".format(hostname)
