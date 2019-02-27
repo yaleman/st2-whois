5b2a1b2532797a78fba88eca1fe9723f8a37c542
yaleman <yaleman@ricetek.net>
2019-02-27 20:34:28 +1000

    lists of bad things!

--- actions/action-whois.py
+++ actions/action-whois.py
@@ -31,11 +31,16 @@ class Whois(Action):
         else:
             self.logger.debug("whois on domain '{}'".format(parsed_uri.netloc))
             w = whois(parsed_uri.netloc)
+        
         result = {}
         # go through the results and put it in a form that stackstorm can take
         for key in w.keys():
             if isinstance(w[key], datetime): # datetime is bad, m'kay
                 result[key] = str(w[key])
+            # because sometimes, people make lists of problematic elements!
+            elif isinstance(w[key], list):
+                if len([element for element in w[key] if isinstance(element, datetime) or isinstance(element, unicode)]) > 0:
+                    result[key] = [str(element) for element in w[key]]
             elif isinstance(w[key], unicode): # something can't handle the unicode conversion...
                 result[key] = str(w[key])
             # everything else I got was OK... 
