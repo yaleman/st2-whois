252d76293a0ef49906d046fc8c303d185e3a96ac
yaleman <yaleman@ricetek.net>
2019-02-28 11:28:59 +1000

    cleaning lists is handy too

--- actions/whois_lib.py
+++ actions/whois_lib.py
@@ -1,7 +1,24 @@
+from datetime import datetime
 
 def clean_list(data):
+    """ filters a list for bad stackstorm items
+    input: list
+    returns: list
+    """
+    result = []
+    for element in data:
+        if isinstance(element, unicode) or isinstance(element, datetime):
+            result.append(str(element))
+        else:
+            result.append(element)
+    return result
+
+def clean_dict(data):
     """ takes any dict, but typically the return value of an Action, and cleans up any data that'll make
-        stackstorm have a hissy fit """
+        stackstorm have a hissy fit
+        input: dict
+        returns: dict
+        """
     result = {}
     # go through the results and put it in a form that stackstorm can take
     for key in data.keys():
@@ -10,7 +27,11 @@ def clean_list(data):
         # because sometimes, people make lists of problematic elements!
         elif isinstance(data[key], list):
             result[key] = clean_list(data[key])
-        elif isinstance(w[key], unicode): # something can't handle the unicode conversion...
+        # handle nested dicts
+        elif isinstance(data[key], dict):
+            result[key] = clean_dict(data[key])
+        # handle unicode, because python2
+        elif isinstance(w[key], unicode):
             result[key] = str(w[key])
         # everything else I got was OK... 
         else:
