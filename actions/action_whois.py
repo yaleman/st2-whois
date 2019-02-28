f51d4d67c8b1f15644606be11483c3cac1351f25
yaleman <yaleman@ricetek.net>
2019-02-28 11:12:45 +1000

    moving lib function to its own file

--- actions/action_whois.py
+++ actions/action_whois.py
@@ -3,31 +3,15 @@
 import re
 from datetime import datetime
 
-from st2common.content import utils
-from st2common.runners.base_action import Action
+if __name__ != '__main__':
+    from st2common.content import utils
+    from st2common.runners.base_action import Action
 
 import whois
 
-
 from urlparse import urlparse
 
-def clean_list(data):
-    """ takes any dict, but typically the return value of an Action, and cleans up any data that'll make
-        stackstorm have a hissy fit """
-    result = {}
-    # go through the results and put it in a form that stackstorm can take
-    for key in data.keys():
-        if isinstance(data[key], datetime): # datetime is bad, m'kay
-            result[key] = str(data[key])
-        # because sometimes, people make lists of problematic elements!
-        elif isinstance(data[key], list):
-            result[key] = clean_list(data[key])
-        elif isinstance(w[key], unicode): # something can't handle the unicode conversion...
-            result[key] = str(w[key])
-        # everything else I got was OK... 
-        else:
-            result[key] = w[key]
-    return result
+from whois_lib import clean_list
 
 class Whois(Action):
     def run(self, query, cmd, *args):
