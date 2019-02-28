2b16166d01eb689746010594088c0c0557ec409a
yaleman <yaleman@ricetek.net>
2019-02-28 10:53:40 +1000

    moving cleanup code out of action

--- actions/action-whois.py
+++ actions/action-whois.py
@@ -9,7 +9,25 @@ from st2common.runners.base_action import Action
 import whois
 
 
-from urlparse import urlparse
+from urlparse import 
+
+def clean_list(data):
+    """ takes any dict, but typically the return value of an Action, and cleans up any data that'll make
+        stackstorm have a hissy fit """
+    result = {}
+    # go through the results and put it in a form that stackstorm can take
+    for key in data.keys():
+        if isinstance(data[key], datetime): # datetime is bad, m'kay
+            result[key] = str(data[key])
+        # because sometimes, people make lists of problematic elements!
+        elif isinstance(data[key], list):
+            result[key] = clean_list(data[key])
+        elif isinstance(w[key], unicode): # something can't handle the unicode conversion...
+            result[key] = str(w[key])
+        # everything else I got was OK... 
+        else:
+            result[key] = w[key]
+    return result
 
 class Whois(Action):
     def run(self, query, cmd, *args):
@@ -38,19 +56,6 @@ class Whois(Action):
         if w.get('status', False) == False:
             return (False, {'error' : "No result returned"})
 
-        result = {}
-        # go through the results and put it in a form that stackstorm can take
-        for key in w.keys():
-            if isinstance(w[key], datetime): # datetime is bad, m'kay
-                result[key] = str(w[key])
-            # because sometimes, people make lists of problematic elements!
-            elif isinstance(w[key], list):
-                if len([element for element in w[key] if isinstance(element, datetime) or isinstance(element, unicode)]) > 0:
-                    result[key] = [str(element) for element in w[key]]
-            elif isinstance(w[key], unicode): # something can't handle the unicode conversion...
-                result[key] = str(w[key])
-            # everything else I got was OK... 
-            else:
-                result[key] = w[key]
+        result = clean_list(w)
         result['text'] = w.text
         return (True, result)
